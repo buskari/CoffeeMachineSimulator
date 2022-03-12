879ee73b8ce18cb58009d3af985a13e5f2bbf943
Andris Buscariolli <andris.contact@gmail.com>
2022-03-12 19:45:55 -0300

    add feature to calculate the amount of ingredients

--- src/machine/CoffeeMachine.java
+++ src/machine/CoffeeMachine.java
@@ -1,18 +1,36 @@
 package machine;
 
-        /*
-        * v1.0
-        * Printing steps to make a coffee
-        * */
+    /*
+    * v1.1
+    * Calculate the amount of ingredients
+    *
+    * 1 cup of coffee
+    *   200 ml of watter
+    *   50 ml of milk
+    *   15g of coffee beans
+    *
+    * */
+
+import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        Scanner scanner = new Scanner(System.in);
+        int cupsOfCoffee;
+        int water = 200;
+        int milk = 50;
+        int coffee = 15;
+
+        System.out.println("Write how many cups of coffee you will need:");
+        cupsOfCoffee = scanner.nextInt();
+
+        water *= cupsOfCoffee;
+        milk *= cupsOfCoffee;
+        coffee *= cupsOfCoffee;
+
+        System.out.println("For " + cupsOfCoffee + " cups of coffee you will need: ");
+        System.out.println(water + " ml of water");
+        System.out.println(milk + " ml of milk");
+        System.out.println(coffee + "g of coffee");
     }
 }
